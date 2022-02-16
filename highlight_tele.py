decca02b01616455aa28b190b6c5aa9f2b73ba49
smeenakshi1997 <smeenakshi1997@gmail.com>
2022-02-16 13:09:04 +0530

    Update highlight_tele.py

--- highlight_tele.py
+++ highlight_tele.py
@@ -7,7 +7,6 @@ Created on Sun Feb 13 19:35:23 2022
 import pytesseract 
 import argparse
 import cv2
-from pytesseract import Output
  
 # We construct the argument parser
 # and parse the arguments
@@ -25,59 +24,37 @@ args = vars(ap.parse_args())
 # it to RGB from BGR. We then use Tesseract
 # to localize each area of text in the input
 # image
-images = cv2.imread(args["image"])
-(h, w, c) = images.shape[:3]
-rgb = cv2.cvtColor(images, cv2.COLOR_BGR2RGB)
-results = pytesseract.image_to_data(rgb, output_type=Output.DICT)
- 
-# Then loop over each of the individual text
-# localizations
-for i in range(0, len(results["text"])):
-     
-    # We can then extract the bounding box coordinates
-    # of the text region from  the current result
-    x = results["left"][i]
-    y = results["top"][i]
-    w = results["width"][i]
-    h = results["height"][i]
-     
-    # We will also extract the OCR text itself along
-    # with the confidence of the text localization
-    text = results["text"][i]
-    conf = int(float(results["conf"][i]))
-     
-    # filter out weak confidence text localizations
-    if conf > args["min_conf"]:
-         
-        # We will display the text to
-        # our terminal
-        t1 = str(x) +"+"+ str(y)
-        t2 = str(x+w) +"+"+ str(y+h)
-        coor = t1+","+t2
-        
-        #print("Confidence: {}".format(conf))
-        #print("Text: {}".format(text))
-        #print("")
-         
-        # We then strip out non-ASCII text so we can
-        # draw the text on the image We will be using
-        # OpenCV, then draw a bounding box around the
-        # text along with the text itself
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (9,9), 0)
+thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,30)
+
+# Dilate to combine adjacent text contours
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30,30))
+dilate = cv2.dilate(thresh, kernel, iterations=4)
+
+# Find contours, highlight text areas, and extract ROIs
+cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+ROI_number = 0
+for c in cnts:
+    area = cv2.contourArea(c)
+    if area > 10000:
+        x,y,w,h = cv2.boundingRect(c)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
+        ROI = image[y:y+h, x:x+w]
+        cv2.imwrite('Results/ROI_{}.png'.format(ROI_number), ROI)
+        text = pytesseract.image_to_string(ROI)
         text = "".join(text).strip()
-        cv2.rectangle(images,
-                      (x, y),
-                      (x + w, y + h),
-                      (0, 0, 255), 2)
-        cv2.putText(images,
-                    coor,
-                    (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1.2, (0, 255, 255), 3)
-
-        print (text)
-# After all, we will show the output image
-cv2.imwrite("newimage.jpg", images)
-cv2.imshow("Image", images)
-cv2.waitKey(0)
-
-#num2words.num2words(123456, lang='en_IN')
+        if "Bharat" in text:
+            #list_of_words = text.split()
+            #next_word = list_of_words[list_of_words.index("Period") + 1]+" "+list_of_words[list_of_words.index("Period") + 2]+" "+list_of_words[list_of_words.index("Period") + 3]
+            print (text)
+        
+        ROI_number += 1
+        
+#cv2.imshow('thresh', thresh)
+#cv2.imshow('dilate', dilate)
+#cv2.imshow('image', image)
+#cv2.waitKey()
